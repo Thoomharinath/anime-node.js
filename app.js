8e1f05a10a019ef2a699f1ecc4ab5d38bc93f12b
harinath <harinathreddythoom@gmail.com>
2021-10-01 14:42:33 +0000

    added-cors

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 
 const dbPath = path.join(__dirname, "reviews.db");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 let db = null;
 
